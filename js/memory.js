7335a77ec60785c527d05115909eab640a0507d8
JacopoJC <jacopo.colleluori@student.univaq.it>
2022-04-09 18:22:03 +0200

    altre variazioni

--- js/memory.js
+++ js/memory.js
@@ -25,93 +25,98 @@ function shuffle(a) {
     }
     return a;
 }
+//Document Ready
+$(() => {
 
-function startGame() {
-    numeroClick = 0
-    $(".numeroClick").text("Click: " + numeroClick)
-    let cards = shuffle(mieImg);
-    let container =$("#griglia");
-    container.text("");
-    cards.forEach((value) => {
-        let wrapper = $("<div> </div>")
-        let card =  $("<img />").attr('src', "./img/" + value + ".png")
-        card.src = "./img/" + value + ".png"
-        card.addClass("icon") 
-        card.attr("draggable", false)
-        card.on("click", displayIcon)
-        card.appendTo(wrapper)
-        wrapper.appendTo("#griglia")
-    })
-}
+    //Funzione inizio gioco
+    function startGame() {
+        numeroClick = 0;
+        $(".numeroClick").text("Click: " + numeroClick);
+        let cards = shuffle(mieImg);
+        let container = $("#griglia");
+        container.text("");
+        cards.forEach((value) => {
+            let wrapper = $("<div> </div>");
+            let card = $("<img />").attr('src', "./img/" + value + ".png");
+            card.src = "./img/" + value + ".png";
+            card.addClass("icon");
+            card.attr("draggable", false);
+            card.on("click", displayIcon);
+            card.appendTo(wrapper);
+            wrapper.appendTo("#griglia");
+        })
+    }
 
-$("#altraPar").on("click",playAgain)
 
-function displayIcon() {
-    numeroClick++
-    $(".numeroClick").text("Click: "+numeroClick)
-    var icon = $(".icon");
-    var icons = [...icon];
-    $(this).parent().toggleClass("show");
-    $(this).parent().toggleClass("disabled");
-    $(this).toggleClass("show");
-   
-    arrayComparison.push(this);
-    var len = arrayComparison.length;
 
-    if (len === 2) {
-        if (arrayComparison[0].src === arrayComparison[1].src) {
-           $(arrayComparison[0]).parent().addClass("find disabled");
-            $(arrayComparison[1]).parent().addClass("find disabled");
-           $(arrayComparison[0]).addClass("find disabled");
-            $(arrayComparison[1]).addClass("find disabled");
-            iconsFind = [...iconsFind, ...arrayComparison]
-            arrayComparison = [];
-            if (iconsFind.length == mieImg.length) {
-                vittoria()
-            }
-        } else {
+    function displayIcon() {
+        numeroClick++
+        $(".numeroClick").text("Click: " + numeroClick)
+        var icon = $(".icon");
+        var icons = [...icon];
+        $(this).parent().toggleClass("show disabled");
+        $(this).toggleClass("show");
 
-            icons.forEach(function(item) {
-                $(item).addClass('disabled');
-            });
+        arrayComparison.push(this);
+        var len = arrayComparison.length;
 
-            setTimeout(function() {
-                $(arrayComparison[0]).parent().removeClass("show");
-                $(arrayComparison[1]).parent().removeClass("show");
-                $(arrayComparison[0]).parent().removeClass("disabled");
-                $(arrayComparison[1]).parent().removeClass("disabled");
-                $(arrayComparison[0]).removeClass("show");
-                $(arrayComparison[1]).removeClass("show");
-                $(arrayComparison[0]).removeClass("disabled");
-                $(arrayComparison[1]).removeClass("disabled");
-                icons.forEach(function(item) {
-                    $(item).removeClass('disabled');
-                    for (var i = 0; i < iconsFind.length; i++) {
-                        $(iconsFind[i]).addClass("disabled");
-                    }
-                });
+        if (len === 2) {
+            if (arrayComparison[0].src === arrayComparison[1].src) {
+                $(arrayComparison[0]).parent().addClass("find disabled");
+                $(arrayComparison[1]).parent().addClass("find disabled");
+                $(arrayComparison[0]).addClass("find disabled");
+                $(arrayComparison[1]).addClass("find disabled");
+                iconsFind = [...iconsFind, ...arrayComparison]
                 arrayComparison = [];
-            }, 700);
+                if (iconsFind.length == mieImg.length) {
+                    vittoria();
+                }
+            } else {
+
+                icons.forEach(function (item) {
+                    $(item).addClass('disabled');
+                });
+
+                setTimeout(function () {
+                    $(arrayComparison[0]).parent().removeClass("show disabled");
+                    $(arrayComparison[1]).parent().removeClass("show disabled");
+                   
+                    $(arrayComparison[0]).removeClass("show disabled");
+                    $(arrayComparison[1]).removeClass("show disabled");
+                   
+                    icons.forEach(function (item) {
+                        $(item).removeClass('disabled');
+                        for (var i = 0; i < iconsFind.length; i++) {
+                            $(iconsFind[i]).addClass("disabled");
+                        }
+                    });
+                    arrayComparison = [];
+                }, 700);
+            }
         }
     }
-}
 
-
-
-function vittoria() {
-   $("#modal").addClass("active")
-   $("numeroClick").text("Click: " + numeroClick)
-    iconsFind = []
-    numeroClick = 0
-}
-
-
-function playAgain() {
-   $("#modal").removeClass("active")
-    startGame()
     
-}
+    function vittoria() {
+        $("#modal").addClass("active");
+        $("numeroClick").text("Click: " + numeroClick);
+        iconsFind = [];
+        numeroClick = 0;
+    }
 
-$(()=>{
-    startGame()
+
+    function playAgain() {
+        $("#modal").removeClass("active");
+        startGame();
+    }
+
+    // Aggiungiamo gli event Listener ai bottoni
+    $(".ricarica").on("click", () => {
+        location.reload();
+    })
+
+    $("input[value=Ricomincia]").on("click", startGame);
+
+    $("#altraPar").on("click", playAgain);
+    startGame();
 })
